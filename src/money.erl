fd61075009a39e1ce9dc9f451bc5edd4e94cb280
Samir Anghan <samir.anghan@gmail.com>
2019-06-13 15:34:27 -0400

    Read customer.txt and banks.txt files, print data from file.

---
+++ src/money.erl
@@ -0,0 +1,42 @@
+%%%-------------------------------------------------------------------
+%%% @author Samir
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13. Jun 2019 14:10
+%%%-------------------------------------------------------------------
+-module(money).
+-author("Samir").
+
+%% API
+-export([master/0]).
+
+master() ->
+  register(master, self()),
+  readCustomersFromFile(),
+  readBanksFromFile().
+
+readCustomersFromFile() ->
+  {ok, Customers} = file:consult("customers.txt"),
+  io:fwrite("** Customers and loan objctives **~n"),
+  CustomersIterator = fun(CustomerElement) -> spawnCustomers(CustomerElement) end,
+  lists:foreach(CustomersIterator, Customers),
+  io:fwrite("~n").
+
+spawnCustomers(Customer) ->
+  Name = element(1, Customer),
+  LoanObjective = element(2, Customer),
+  io:fwrite("~w: ~w~n", [Name, LoanObjective]).
+
+readBanksFromFile() ->
+  {ok, Banks} = file:consult("banks.txt"),
+  io:fwrite("** Banks and financial resources **~n"),
+  BanksIterator = fun(BankElement) -> spawnBanks(BankElement) end,
+  lists:foreach(BanksIterator, Banks),
+  io:fwrite("~n").
+
+spawnBanks(Bank) ->
+  Name = element(1, Bank),
+  FinancialResources = element(2, Bank),
+  io:fwrite("~w: ~w~n", [Name, FinancialResources]).
